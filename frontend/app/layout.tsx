e1ad78b0b371c3eee94bcb3633baab372019ab55
Ronaldo Segundo Jr <ronaldosegundojr@usp.br>
2024-10-02 21:20:53 -0300

    inicio do projeto

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import  {Montserrat} from 'next/font/google'
+import "./globals.css";
+
+const font = Montserrat({
+  subset: 'latin',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: "Gamer.store", // Titulo da aba do navegador
+  description: "Versao completa da loja", // Descrição do site
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={font.className}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
